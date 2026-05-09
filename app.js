53a1d26a2955efaeffbc1130225d06087ece7534
YAWO KODJO <yawo.kodjo@yahoo.com>
2026-05-09 16:20:11 +0000

    add in app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Hello, Git!");
+console.log("here comes trouble")
+
+//git commit -ma is a shortcut for git add . and git commit -m "message" in one command. It stages all changes and commits them with the provided message. 
