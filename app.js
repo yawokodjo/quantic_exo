1e4d51faf32b5f9921a78710370b50fe485efc83
YAWO KODJO <yawo.kodjo@yahoo.com>
2026-05-09 22:48:27 +0000

    default commit message template:

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
 console.log("Hello, Git!");
-console.log("here comes trouble")
-alert("uh-oh!");
-
-//git commit -ma is a shortcut for git add . and git commit -m "message" in one command. It stages all changes and commits them with the provided message. 
-//echo "console.log('Nouveau code ajouté depuis la console!');" >> app.js
-//echo "console.log('Contenu entièrement remplacé!');" > app.js
